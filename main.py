f26a682dc918c6da4d2f2fee4d14415c97ce694f
jorgepiao <piaosin11@gmail.com>
2020-08-27 22:35:52 -0500

    Interaccion con pestañas (mover, cerrar).. y barra de direcciones

--- main.py
+++ main.py
@@ -9,27 +9,52 @@ from PyQt5.QtGui import QIcon, QWindow, QImage
 from PyQt5.QtCore import *
 
 
+class BarraDirecciones(QLineEdit): # AddressBar()
+    def __init__(self):
+        super().__init__()
+
+    def mousePressEvent(self, e):
+        self.selectAll()
+
+
 class App(QFrame):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Navegador Web')
-        self.setBaseSize(1366, 768)
-        # self.CreateApp()
         self.CrearApp()
+        self.setBaseSize(1366, 768)
 
-    def CrearApp(self):
+    def CrearApp(self): # CreateApp()
         self.layout = QVBoxLayout()
-        self.tabbar = QTabBar()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0,0,0,0)
+
+        #- crear pestañas
+        self.tabbar = QTabBar(movable=True, tabsClosable=True)
+        self.tabbar.tabCloseRequested.connect(self.CerrarTab)
 
         self.tabbar.addTab('Pestaña 1')
         self.tabbar.addTab('Pestaña 2')
 
         self.tabbar.setCurrentIndex(0)
 
+        #- crear barra de direcciones
+        self.Toolbar = QWidget()
+        self.ToolbarLayout = QHBoxLayout()
+        self.addressbar = BarraDirecciones()
+
+        self.Toolbar.setLayout(self.ToolbarLayout)
+        self.ToolbarLayout.addWidget(self.addressbar)
+
         self.layout.addWidget(self.tabbar)
+        self.layout.addWidget(self.Toolbar)
         self.setLayout(self.layout)
 
         self.show()
+
+
+    def CerrarTab(self, i): # CloseTab()
+        self.tabbar.removeTab(i)
 
 
 if __name__ == "__main__":
